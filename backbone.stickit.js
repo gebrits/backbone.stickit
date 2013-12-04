ecd7c761c2b8f1065f52e81f4daa0722b27cd193
Geert-Jan Brits <info@clearskyabove.com>
2013-12-04 20:19:41 +0100

    add option alwaysUpdateView

--- backbone.stickit.js
+++ backbone.stickit.js
@@ -113,7 +113,8 @@
           _.each(_.flatten([modelAttr]), function(attr) {
             observeModelEvent(model, this, 'change:'+attr, function(model, val, options) {
               var changeId = options && options.stickitChange && options.stickitChange.bindId || null;
-              if (changeId !== bindId)
+              if (changeId !== bindId || options.alwaysUpdateView)
+                console.log("alwaysUpdateView: " + options.alwaysUpdateView);
                 updateViewBindEl(this, $el, config, getAttr(model, modelAttr, config, this), model);
             });
           }, this);
